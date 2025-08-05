3be6ba749e94e29e28c5002408e21c66f4215bd0
coyomii <2995265803@qq.com>
2025-08-05 16:41:25 +0800

    style: 调整导入顺序

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 def get_browser_options():
     options = webdriver.EdgeOptions()
